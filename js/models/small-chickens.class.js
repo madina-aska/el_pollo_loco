dfbfcccf95c06393987994ea2d9bed88abfdafe7
Madina Askar Zada <madin.aska@gmail.com>
2025-07-28 12:26:53 +0200

    small enemies added

---
+++ js/models/small-chickens.class.js
@@ -0,0 +1,34 @@
+class Smallchickens extends MovableObject {
+   y = 360;
+   height = 60;
+   width = 60;
+   IMAGES_WALKING = [
+    'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+    'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+    'img/3_enemies_chicken/chicken_small/1_walk/3_w.png'   
+   ];
+   
+    constructor() {
+        super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
+        this.loadImages(this.IMAGES_WALKING);
+
+        this.x = 200 + Math.random() * 500;              //The Math.random() method returns a random floating point number between 0 (inclusive) and 1 (exclusive).
+        this.speed = 0.15 + Math.random() * 0.5;
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.moveLeft();
+            
+            }, 1000/60);
+        
+
+        setInterval( () => {
+           this.playAnimation(this.IMAGES_WALKING);
+        }, 200);
+
+
+}
+}
