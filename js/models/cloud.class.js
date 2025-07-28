82ce65466ba3588c517ca955bec166be389c3f5f
Madina Askar Zada <madin.aska@gmail.com>
2025-07-28 12:26:24 +0200

    coulds added

--- js/models/cloud.class.js
+++ js/models/cloud.class.js
@@ -2,19 +2,22 @@ class Cloud extends MovableObject {
     y = 20;
     width = 500;
     height = 250;
-  
 
-    constructor() {
-        super().loadImage('img/5_background/layers/4_clouds/1.png');
-        
-        this.x = Math.random() * 500;
+    IMAGES = [
+        'img/5_background/layers/4_clouds/1.png',
+        'img/5_background/layers/4_clouds/2.png'
+    ];
+  
+    constructor(x = 0, imageIndex = 0) {
+        super().loadImage(this.IMAGES[imageIndex % this.IMAGES.length]);
+        this.loadImages(this.IMAGES);
+        this.x = x;
         this.animate();
-         
     }
 
     animate() {
-        this.moveLeft();
+        setInterval(() => {
+            this.moveLeft();
+        }, 1000/60);
     }
-
-  
-} 
+}
