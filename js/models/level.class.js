9d4ff2a2f3e8112c6a4d8f3518121cefd10a8741
Madina Askar Zada <madin.aska@gmail.com>
2025-07-30 17:17:15 +0200

    Updated Level class

--- js/models/level.class.js
+++ js/models/level.class.js
@@ -1,15 +1,21 @@
-class Level {
-    enemies;
-    clouds;
-    backgroundObjects;
-    level_end_x = 2200;
-    endboss;
 
-    constructor(enemies, clouds, backgroundObjects, endboss) {
+// class Level: bundles together all the components that make up the level, including:
+//backgroundObjects, clouds, enemies, endboss
+
+
+
+class Level {                                       
+    backgroundObjects;
+    clouds;
+    enemies;
+    endboss;
+    level_end_x = 2900;
+
+    constructor(backgroundObjects, clouds, enemies, endboss) {
+        this.backgroundObjects = backgroundObjects;
         this.clouds = clouds;
         this.enemies = enemies;
         this.endboss = endboss;
-        this.backgroundObjects = backgroundObjects;
-
+        
     }
 }
