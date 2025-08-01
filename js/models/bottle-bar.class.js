25b297a3ac6a6bea7b9c3edecc96684e837caa66
Madina Askar Zada <madin.aska@gmail.com>
2025-08-01 22:48:51 +0200

    coinbar and bottlebar updated

--- js/models/bottle-bar.class.js
+++ js/models/bottle-bar.class.js
@@ -1,36 +1,33 @@
 class BottleBar extends StatusBar {
-  x = 10;
-  y = 100;
-  IMAGES = [
-    "img/7_statusbars/1_statusbar/3_statusbar_bottle/orange/0.png",
-    "img/7_statusbars/1_statusbar/3_statusbar_bottle/orange/20.png",
-    "img/7_statusbars/1_statusbar/3_statusbar_bottle/orange/40.png",
-    "img/7_statusbars/1_statusbar/3_statusbar_bottle/orange/60.png",
-    "img/7_statusbars/1_statusbar/3_statusbar_bottle/orange/80.png",
-    "img/7_statusbars/1_statusbar/3_statusbar_bottle/orange/100.png",
+   IMAGES = [
+    'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png',
+    'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png',
+    'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/40.png',
+    'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/60.png',
+    'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/80.png',
+    'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/100.png'
   ];
 
   percentage = 100;
 
     constructor() {
-        super();
+        super().loadImage(this.IMAGES[5]);
         this.loadImages(this.IMAGES);
         this.x = 40;
         this.y = 100;
         this.width = 200;
         this.height = 60;
-        this.setPercentage(100);
     }
     
 
     setPercentage(percentage) {
-        this.percentage = percentage;   // => 0...5
+        this.percentage = percentage;
         let path = this.IMAGES[this.resolveImageIndex()];
         this.img = this.imageCache[path];
     }
 
     resolveImageIndex() {
-        if (this.percentage == 100) {
+        if (this.percentage === 100) {
             return 5;
         } else if (this.percentage > 80) {
             return 4;
--- js/models/coin-bar.class.js
+++ js/models/coin-bar.class.js
@@ -1,35 +1,35 @@
 class CoinBar extends StatusBar {
  
   IMAGES = [
-    "img/7_statusbars/1_statusbar/1_statusbar_coin/green/0.png",
-    "img/7_statusbars/1_statusbar/1_statusbar_coin/green/20.png",
-    "img/7_statusbars/1_statusbar/1_statusbar_coin/green/40.png",
-    "img/7_statusbars/1_statusbar/1_statusbar_coin/green/60.png",
-    "img/7_statusbars/1_statusbar/1_statusbar_coin/green/80.png",
-    "img/7_statusbars/1_statusbar/1_statusbar_coin/green/100.png",
+        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/0.png',
+        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/20.png',
+        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/40.png',
+        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/60.png',
+        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/80.png',
+        './img/7_statusbars/1_statusbar/1_statusbar_coin/blue/100.png'
   ];
 
   percentage = 100;
 
     constructor() {
-        super();
+        super().loadImage(this.IMAGES[5]);
         this.loadImages(this.IMAGES);
+
         this.x = 40;
         this.y = 50;
         this.width = 200;
         this.height = 60;
-        this.setPercentage(100);
     }
     
 
     setPercentage(percentage) {
-        this.percentage = percentage;   // => 0...5
+        this.percentage = percentage;
         let path = this.IMAGES[this.resolveImageIndex()];
         this.img = this.imageCache[path];
     }
 
     resolveImageIndex() {
-        if (this.percentage == 100) {
+        if (this.percentage === 100) {
             return 5;
         } else if (this.percentage > 80) {
             return 4;
