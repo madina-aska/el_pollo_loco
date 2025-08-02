c448b8611239ad7c352c9f841e5fade909886364
Madina Askar Zada <madin.aska@gmail.com>
2025-08-02 22:57:01 +0200

    salsa bottles are added

---
+++ js/models/bottle-two.class.js
@@ -0,0 +1,19 @@
+
+//Defines a ground salsa bottle collectible with fixed size and Y-position, 
+//placed at a random X-position and rendered using a specific image.
+
+class BottleTwo extends DrawableObject {
+    y = 350;
+    width = 65;
+    height = 80;
+
+    IMAGES_BOTTLE = [
+        'img/6_salsa_bottle/2_salsa_bottle_on_ground.png'
+    ];
+
+    constructor() {
+        super().loadImage(this.IMAGES_BOTTLE[0]);
+        this.x = 300 + Math.random() * 1500;
+      
+    }
+}
--- js/models/level.class.js
+++ js/models/level.class.js
@@ -1,7 +1,6 @@
 
 // class Level: bundles together all the components that make up the level, including:
-//backgroundObjects, clouds, enemies, endboss
-
+//backgroundObjects, clouds, enemies, endboss, coins and bottles
 
 
 class Level {                                       
@@ -9,13 +8,17 @@ class Level {
     clouds;
     enemies;
     endboss;
+    coins;
+    bottles;
     level_end_x = 2900;
 
-    constructor(backgroundObjects, clouds, enemies, endboss) {
+    constructor(backgroundObjects, clouds, enemies, endboss, coins, bottles) {
         this.backgroundObjects = backgroundObjects;
         this.clouds = clouds;
         this.enemies = enemies;
         this.endboss = endboss;
+        this.coins = coins;
+        this.bottles = bottles;
         
     }
 }
