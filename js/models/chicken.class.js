2299c451765064fcfed0a0fc4cc47d9f0040de22
Madina Askar Zada <madin.aska@gmail.com>
2025-08-03 22:53:21 +0200

    Add Chickens with movement, animation, and defeat logic

--- js/models/chicken.class.js
+++ js/models/chicken.class.js
@@ -1,47 +1,65 @@
-class Chicken extends MovableObject {
-   y = 360;
-   height = 60;
-   width = 100;
-    offset = {
-    top: 5,
-    bottom: 10,
-    left: 10,
-    right: 5,
-  };
+class Chicken extends MovableObject {    
+    y = 330;
+    width = 90;
+    height = 100;
+   
    IMAGES_WALKING = [
     '/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
     'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
     'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
    ];
 
-   IMAGES_DEAD = [
-    'img/3_enemies_chicken/chicken_normal/2_dead/dead.png',
-    'img/3_enemies_chicken/chicken_normal/2_dead/dead.png'
-  ];
-   
-    constructor() {
+
+    constructor(x) {
         super().loadImage('/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
-        this.loadImages(this.IMAGES_DEAD);
-
-        this.x = 200 + Math.random() * 500;              //The Math.random() method returns a random floating point number between 0 (inclusive) and 1 (exclusive).
-        this.speed = 0.15 + Math.random() * 0.5;
-
+        this.x = x;
+        this.speedX = 0.5 + Math.random() * 0.8;              
         this.animate();
     }
-
+    
+    // Start animation and movement intervals
     animate() {
-        setInterval(() => {
-            this.moveLeft();
-            
-            }, 1000/60);
-
-        setInterval( () => {
-           this.playAnimation(this.IMAGES_WALKING);
-        }, 200);
-
-
+        this.animationIntervals.push(
+            setInterval(() => this.startAnimations(), 200));
+        this.animationIntervals.push(
+            setInterval(() => this.moveLeft(), 1000 / 60));
     }
 
+   // Play walking animation if game is active
+    startAnimations() {
+        if (!gamePaused && gameStarted) {
+            this.playAnimation(this.IMAGES_WALKING);
+        }
+    }
 
+    // Move the Enemy left if game is active
+    moveLeft() {
+        if (!gamePaused && gameStarted) {
+            super.moveLeft();
+        }
+    }
+    
+    // Triggered when the Enemy is defeated
+    eliminateEnemy() {
+        this.energy = 0;
+        this.stopAnimations();
+        this.img.src = 'img/3_enemies_chicken/chicken_normal/2_dead/dead.png';
+        this.enableRemoval();
+    }
+   
+    // Allow the enemies to be removed after a delay
+    enableRemoval() {
+        setTimeout(() => {
+            this.canBeRemoved = true;
+        }, 400);
+    }
+
+    // Clear all running animation intervals
+    stopAnimations() {
+        this.animationIntervals.forEach(intervalId => {
+        clearInterval(intervalId); 
+       });
+        this.animationIntervals = [];
+    }
 }
