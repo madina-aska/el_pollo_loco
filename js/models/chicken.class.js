fa609aa59c475440835053a084b0eeae28c9c136
Madina Askar Zada <madin.aska@gmail.com>
2025-08-06 22:14:05 +0200

    Fix false image path

--- js/models/chicken.class.js
+++ js/models/chicken.class.js
@@ -4,14 +4,14 @@ class Chicken extends MovableObject {
     height = 100;
    
    IMAGES_WALKING = [
-    '/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+    'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
     'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
     'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
    ];
 
 
     constructor(x) {
-        super().loadImage('/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+        super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
         this.x = x;
         this.speedX = 0.5 + Math.random() * 0.8;              
